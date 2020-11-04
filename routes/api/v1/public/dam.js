27baaecd12e1dc9f971e6a96a4a56930acf036f4
李玉刚 <liyugang@liyugangs-Mac-mini.local>
2020-11-04 17:24:11 +0800

    接口增加操作类型

--- routes/api/v1/public/dam.js
+++ routes/api/v1/public/dam.js
@@ -20,7 +20,6 @@ router.get('/', async (ctx, next) => {
         })
     })
 
-
     ctx.sendResult({ data: res.toString('hex') }, 200, '指令发送成功')
 
     next()
@@ -32,27 +31,34 @@ router.get('/equipopr', async (ctx, next) => {
     //  设备地址
     let equipAddr = parseInt(shopid).toString(16).padStart(2, 0)
     //  操作通道
-    let ch = channel ? openRelay.ch0 : openRelay.ch1
+    let ch = ''
+    if (oprtype === 'open') {
+        ch = channel ? openRelay.ch1 : openRelay.ch0
+    } else {
+        ch = channel ? closeRelay.ch1 : closeRelay.ch0
+    }
     //  crc16校验码
     let sum = crc16.checkSum(equipAddr + ch)
-
+    //  拼接操作指令
     let instruction = equipAddr + ch + sum
 
+    console.log(instruction)
+    //  tc = tcpClient 获取tcp服务器中的client
     let tc = await require('../../../../modules/tcpserver').catch(err => {
-        ctx.sendResult({ data: err }, 400, '指令发送失败')
+        ctx.sendResult({ data: err }, 400, '操作失败')
         return
     })
-
+    //  tc写指令
     tc.write(encodeInstruction(instruction))
 
+    //  tcp服务器接收返回指令
     const res = await new Promise(resolve => {
         tc.on('data', data => {
             resolve(data)
         })
     })
-
-
-    ctx.sendResult({ data: res.toString('hex') }, 200, '指令发送成功')
+    //  接口返回数据
+    ctx.sendResult({ shopid, channel, oprtype }, 200, '操作成功')
 
     next()
 })
